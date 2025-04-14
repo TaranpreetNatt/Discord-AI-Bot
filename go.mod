d819fd23ba8be40b31d89ba23ed1b54599bff805
Taranpreet Natt <taranpreetnatt12@hotmail.com>
2025-04-13 19:52:15 -0700

    Initial websocket connection to Discord established

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/taranpreetnatt/Discord-AI-Bot
 go 1.24.2
 
 require (
+	github.com/coder/websocket v1.8.13
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/net v0.38.0
 )
--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"time"
-	// "github.com/coder/websocket"
+
+	"github.com/coder/websocket"
 )
 
 const (
@@ -24,6 +26,17 @@ type GatewayResponse struct {
 	URL               string            `json:"url"`
 	Shards            int               `json:"shards"`
 	SessionStartLimit SessionStartLimit `json:"session_start_limit"`
+}
+
+type HeartBeat struct {
+	HeartBeatInterval int `json:"heartbeat_internal"`
+}
+
+type Payload struct {
+	Op        int             `json:"op"`
+	Data      json.RawMessage `json:"d"`
+	Sequence  int             `json:"s"`
+	EventName string          `json:"t"`
 }
 
 func GetGatewayUrl(ctx context.Context, botToken, apiBase string, client *http.Client) (string, error) {
@@ -67,6 +80,50 @@ func GetGatewayUrl(ctx context.Context, botToken, apiBase string, client *http.C
 	return (gatewayResponse.URL + discordApiVersion), nil
 }
 
+func ParseMessage(data []byte) (*Payload, error) {
+	if data == nil {
+		return nil, fmt.Errorf("Cannot parse nil data slice")
+	}
+
+	if len(data) == 0 {
+		return nil, fmt.Errorf("Cannot parse an empty data slice")
+	}
+
+	var p Payload
+	if err := json.Unmarshal(data, &p); err != nil {
+		return nil, fmt.Errorf("Error parsing data, %v", err)
+	}
+
+	return &p, nil
+}
+
+func WebsocketConnection(ctx context.Context, url string) error {
+	conn, resp, err := websocket.Dial(ctx, url, nil)
+	if err != nil || resp.StatusCode != 101 {
+		return fmt.Errorf("Error creating websocket connection, %v", err)
+	}
+	defer conn.CloseNow()
+
+	for {
+		messageType, reader, err := conn.Reader(ctx)
+		if err != nil {
+			return err
+		}
+
+		if messageType == websocket.MessageText {
+			//TODO: Change from ReadAll to using the reader.read and implement a max message size
+			data, err := io.ReadAll(reader)
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(data))
+
+		}
+	}
+
+	return nil
+}
+
 func StartBot(ctx context.Context, botToken, apiBase string) error {
 	fmt.Println("Starting bot")
 	url, gatewayErr := GetGatewayUrl(ctx, botToken, apiBase, nil)
@@ -74,5 +131,11 @@ func StartBot(ctx context.Context, botToken, apiBase string) error {
 		return gatewayErr
 	}
 	fmt.Println("URL: " + url)
+
+	err := WebsocketConnection(ctx, url)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
