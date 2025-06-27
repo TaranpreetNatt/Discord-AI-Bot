e864652ce653f536d386f3b0b6330c2cd7f4806a
Taranpreet Natt <42503785+TaranpreetNatt@users.noreply.github.com>
2025-06-26 23:40:23 -0700

    Create discord reconnect logic (#10)
    * Added gateway close event codes
    
    * Wrote reconnect logic
    
    * Added more reconnect api specification

--- internal/websocket/discord_websocket.go
+++ internal/websocket/discord_websocket.go
@@ -4,14 +4,43 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/bwmarrin/snowflake"
 	"io"
 	"math/rand/v2"
 	"sync"
 	"time"
 
+	"github.com/bwmarrin/snowflake"
+
 	"github.com/coder/websocket"
 )
+
+type GatewayCloseEventCodes struct {
+	Code        int
+	Description string
+	Explanation string
+	Reconnect   bool
+}
+
+const (
+	discordApiVersion = "/?v=10&encoding=json"
+)
+
+var gatewayCloseEventCodes = map[int]GatewayCloseEventCodes{
+	4000: {Code: 4000, Description: "Unknown error", Explanation: "We're not sure what went wrong. Try reconnecting?", Reconnect: true},
+	4001: {Code: 4001, Description: "Unknown opcode", Explanation: "You sent an invalid Gateway opcode or an invalid payload for an opcode. Don't do that!", Reconnect: true},
+	4002: {Code: 4002, Description: "Decode error", Explanation: "You sent an invalid payload to Discord. Don't do that!", Reconnect: true},
+	4003: {Code: 4003, Description: "Not authenticated", Explanation: "You sent us a payload prior to identifying, or this session has been invalidated.", Reconnect: true},
+	4004: {Code: 4004, Description: "Authentication failed", Explanation: "The account token sent with your identify payload is incorrect.", Reconnect: false},
+	4005: {Code: 4005, Description: "Already authenticated", Explanation: "You sent more than one identify payload. Don't do that!", Reconnect: true},
+	4007: {Code: 4007, Description: "Invalid seq", Explanation: "The sequence sent when resuming the session was invalid. Reconnect and start a new session.", Reconnect: true},
+	4008: {Code: 4008, Description: "Rate limited", Explanation: "Woah nelly! You're sending payloads to us too quickly. Slow it down! You will be disconnected on receiving this.", Reconnect: true},
+	4009: {Code: 4009, Description: "Session timed out", Explanation: "Your session timed out. Reconnect and start a new one.", Reconnect: true},
+	4010: {Code: 4010, Description: "Invalid shard", Explanation: "You sent us an invalid shard when identifying.", Reconnect: false},
+	4011: {Code: 4011, Description: "Sharding required", Explanation: "The session would have handled too many guilds - you are required to shard your connection in order to connect.", Reconnect: false},
+	4012: {Code: 4012, Description: "Invalid API version", Explanation: "You sent an invalid version for the gateway.", Reconnect: false},
+	4013: {Code: 4013, Description: "Invalid intent(s)", Explanation: "You sent an invalid intent for a Gateway Intent. You may have incorrectly calculated the bitwise value.", Reconnect: false},
+	4014: {Code: 4014, Description: "Disallowed intent(s)", Explanation: "You sent a disallowed intent for a Gateway Intent. You may have tried to specify an intent that you have not enabled or are not approved for.", Reconnect: false},
+}
 
 type SnowflakeID snowflake.ID
 
@@ -25,6 +54,7 @@ type WebSocketConn struct {
 	Token                       string
 	resumeUrl                   string
 	sessionId                   string
+	url                         string
 }
 
 type HeartbeatInterval struct {
@@ -41,6 +71,17 @@ type Payload struct {
 type Heartbeat struct {
 	Op   int  `json:"op"`
 	Data *int `json:"d"`
+}
+
+type Resume struct {
+	Op   int         `json:"op"`
+	Data *ResumeData `json:"d"`
+}
+
+type ResumeData struct {
+	Token     string `json:"token"`
+	SessionId string `json:"session_id"`
+	Seq       int    `json:"seq"`
 }
 
 type ConnectionProperties struct {
@@ -60,7 +101,9 @@ type IdentifyData struct {
 }
 
 type Identify struct {
-	Op   int          `json:"op"`
+	Op int `json:"op"`
+	//TODO: Make this a pointer
+	//TODO: Refactor the Identify and Heartbeat to be one general struct
 	Data IdentifyData `json:"d"`
 }
 
@@ -164,6 +207,21 @@ func (w *WebSocketConn) GetMessages(ctx context.Context) {
 		default:
 			messageType, reader, err := w.Conn.Reader(ctx)
 			if err != nil {
+
+				//TODO: Disconnect without opcode should reconnect.
+				closeStatusCode := websocket.CloseStatus(err)
+				_, ok := gatewayCloseEventCodes[int(closeStatusCode)]
+				if ok {
+					if gatewayCloseEventCodes[int(closeStatusCode)].Reconnect {
+						fmt.Printf("Reconnecting to discord, statuscode received: %v\n", gatewayCloseEventCodes[int(closeStatusCode)])
+						//reconnect
+						if err := w.reconnect(ctx); err != nil {
+							fmt.Printf("Error reconnecing to discord after Opcode 7: %v\n", err)
+							return
+						}
+						fmt.Printf("Reconnecting to discord after: %+v\n", gatewayCloseEventCodes[int(closeStatusCode)])
+					}
+				}
 				fmt.Printf("Error reading from websocket connection: %v\n", err)
 				return
 			}
@@ -184,6 +242,37 @@ func (w *WebSocketConn) GetMessages(ctx context.Context) {
 				fmt.Printf("Payload: %+v\n", payload)
 				if payload.Op == 0 && payload.Sequence != nil && *payload.Sequence > 0 {
 					w.lastSequence = *payload.Sequence
+				}
+
+				if payload.Op == 7 {
+					if w.sessionId == "" {
+						fmt.Println("Cannot reconnect, no sessionId")
+						return
+					}
+					//reconnect
+					if err := w.reconnect(ctx); err != nil {
+						fmt.Printf("Error reconnecing to discord after Opcode 7: %v", err)
+						return
+					}
+					fmt.Println("Reconnecting to discord, after Opcode 7")
+				}
+
+				if payload.Op == 9 {
+					var shouldResume bool
+					if err := json.Unmarshal(payload.Data, &shouldResume); err != nil {
+						fmt.Printf("Error unmarhsling opcode 9 data in Coordinator: %v", err)
+						return
+					}
+					if shouldResume {
+						// reconnect
+						if err := w.reconnect(ctx); err != nil {
+							fmt.Printf("Error reconnecing to discord after Opcode 9: %v", err)
+							return
+						}
+						fmt.Println("Reconnecting to discord, after Opcode 9")
+					}
+					fmt.Println("Opcode 9 received with reconnect false, disconnecting from websocket")
+					return
 				}
 
 				w.payloadChan <- payload
@@ -237,6 +326,7 @@ func (w *WebSocketConn) PingDiscord(ctx context.Context) {
 		return
 	}
 
+	var reconnectTries int
 	for {
 		select {
 		case <-ctx.Done():
@@ -253,6 +343,7 @@ func (w *WebSocketConn) PingDiscord(ctx context.Context) {
 
 			if payload.Op == 11 {
 				heartbeatAckTimer.Reset(time.Duration(120) * time.Second)
+				reconnectTries = 0
 				continue
 			}
 			if err := w.WriteConn(ctx, heartbeatmessage); err != nil {
@@ -267,8 +358,19 @@ func (w *WebSocketConn) PingDiscord(ctx context.Context) {
 			heartbeatAckTimer.Reset(time.Duration(5) * time.Second)
 			ticker.Reset(setHeartbeatInterval(heartbeatinterval, rand.Float64()))
 		case <-heartbeatAckTimer.C:
-			fmt.Println("No heartbeat response from discord")
-			return
+			if reconnectTries == 3 {
+				fmt.Println("Tried to reconnect to discord three times, shutting down connection")
+				return
+			}
+
+			fmt.Println("No heartbeat response from discord, attempting to reconnect")
+			if err := w.reconnect(ctx); err != nil {
+				fmt.Printf("Error reconnecting after no heartbeakack received: %v\n", err)
+				return
+			}
+			reconnectTries++
+			heartbeatAckTimer.Reset(time.Duration(5) * time.Second)
+			ticker.Reset(setHeartbeatInterval(heartbeatinterval, rand.Float64()))
 		}
 	}
 }
@@ -310,6 +412,59 @@ func (w *WebSocketConn) parseReadyData(data []byte) (*Ready, error) {
 	return &payload, nil
 }
 
+func (w *WebSocketConn) reconnect(ctx context.Context) error {
+	resumeData := ResumeData{
+		Token:     w.Token,
+		SessionId: w.sessionId,
+		Seq:       w.lastSequence,
+	}
+
+	resume := Resume{
+		Op:   6,
+		Data: &resumeData,
+	}
+
+	url := w.resumeUrl
+	if url == "" {
+		url = w.url
+	}
+
+	if err := w.Conn.CloseNow(); err != nil {
+		return fmt.Errorf("Error closing connection before reconnect: %v\n", err)
+	}
+	w.initialHeartbeatAckReceived = false
+
+	maxRetries := 5
+	var attempt int
+out:
+	for attempt = 0; attempt < maxRetries; attempt++ {
+		delay := time.Duration(1<<attempt) * time.Second
+		select {
+		case <-ctx.Done():
+			return fmt.Errorf("Context cancelled during reconnect: %v\n", ctx.Err())
+		case <-time.After(delay):
+			conn, resp, err := websocket.Dial(ctx, url, nil)
+			if err == nil && resp != nil && resp.StatusCode == 101 {
+				w.Conn = conn
+				break out
+			}
+		}
+	}
+
+	if attempt == maxRetries {
+		return fmt.Errorf("Error creating websocket connection\n")
+	}
+
+	resumeByte, err := json.Marshal(resume)
+	if err != nil {
+		return fmt.Errorf("Error marshiling resume payload during Reconnect: %v\n", err)
+	}
+	if err := w.WriteConn(ctx, resumeByte); err != nil {
+		return fmt.Errorf("Error writing to conn during Reconnect: %v\n", err)
+	}
+	return nil
+}
+
 func (w *WebSocketConn) Coordinator(ctx context.Context) {
 	for {
 		select {
@@ -335,7 +490,7 @@ func (w *WebSocketConn) Coordinator(ctx context.Context) {
 					return
 				}
 				fmt.Printf("\nReady Payload: %+v\n", readyPayload)
-				w.resumeUrl = readyPayload.ResumeUrl
+				w.resumeUrl = readyPayload.ResumeUrl + discordApiVersion
 				w.sessionId = readyPayload.SessionId
 			}
 		}
