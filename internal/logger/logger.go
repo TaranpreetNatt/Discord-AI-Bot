366007292f49c82ba14b63de6254c19358c20f21
Taranpreet Natt <taranpreetnatt12@hotmail.com>
2025-07-26 20:57:34 -0700

    Created new logger interface with Zap wrapper for interchangability, also added to main

---
+++ internal/logger/logger.go
@@ -0,0 +1,41 @@
+package logger
+
+type Level int8
+
+const (
+	LevelDebug Level = iota - 1
+	LevelInfo
+	LevelWarn
+	LevelError
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "DEBUG"
+	case LevelInfo:
+		return "INFO"
+	case LevelWarn:
+		return "WARN"
+	case LevelError:
+		return "ERROR"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+type Field struct {
+	Key   string
+	Value any
+}
+
+type Logger interface {
+	Debug(msg string, fields ...Field)
+	Info(msg string, fields ...Field)
+	Warn(msg string, fields ...Field)
+	Error(msg string, fields ...Field)
+	With(fields ...Field) Logger
+	Sync() error
+	Level() Level
+	SetLevel(level Level)
+}
---
+++ internal/logger/zap_adapter.go
@@ -0,0 +1,114 @@
+package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type zapAdapter struct {
+	core  *zap.Logger
+	level zap.AtomicLevel
+}
+
+func (z *zapAdapter) Debug(msg string, fields ...Field) {
+	z.core.Debug(msg, toZapFields(fields)...)
+}
+
+func (z *zapAdapter) Info(msg string, fields ...Field) {
+	z.core.Info(msg, toZapFields(fields)...)
+}
+
+func (z *zapAdapter) Warn(msg string, fields ...Field) {
+	z.core.Warn(msg, toZapFields(fields)...)
+}
+
+func (z *zapAdapter) Error(msg string, fields ...Field) {
+	z.core.Error(msg, toZapFields(fields)...)
+}
+
+func (z *zapAdapter) With(fields ...Field) Logger {
+	return &zapAdapter{core: z.core.With(toZapFields(fields)...), level: z.level}
+}
+
+func (z *zapAdapter) Sync() error {
+	return z.core.Sync()
+}
+
+func (z *zapAdapter) Level() Level {
+	return fromZapLevel(z.level.Level())
+}
+
+func (z *zapAdapter) SetLevel(level Level) {
+	z.level.SetLevel(toZapLevel(level))
+}
+
+// fromZapLevel maps Zap's level back to custom Level.
+func fromZapLevel(l zapcore.Level) Level {
+	switch l {
+	case zap.DebugLevel:
+		return LevelDebug
+	case zap.InfoLevel:
+		return LevelInfo
+	case zap.WarnLevel:
+		return LevelWarn
+	case zap.ErrorLevel:
+		return LevelError
+	default:
+		return LevelInfo
+	}
+}
+
+// toZapLevel maps custom Level to Zap's level.
+func toZapLevel(l Level) zapcore.Level {
+	switch l {
+	case LevelDebug:
+		return zap.DebugLevel
+	case LevelInfo:
+		return zap.InfoLevel
+	case LevelWarn:
+		return zap.WarnLevel
+	case LevelError:
+		return zap.ErrorLevel
+	default:
+		return zap.InfoLevel
+	}
+}
+
+func NewZapAdapter() (Logger, error) {
+	config := zap.NewProductionConfig()
+	config.Encoding = "json"
+	config.Level = zap.NewAtomicLevelAt(zap.InfoLevel)
+	config.Sampling = &zap.SamplingConfig{
+		Initial:    100,
+		Thereafter: 100,
+	}
+	config.OutputPaths = []string{"stdout"}
+	config.ErrorOutputPaths = []string{"stderr"}
+	config.EncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
+	config.EncoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
+
+	z, err := config.Build(zap.AddCaller(), zap.AddStacktrace(zap.ErrorLevel))
+	if err != nil {
+		return nil, err
+	}
+	return &zapAdapter{core: z, level: config.Level}, nil
+}
+
+func toZapFields(fields []Field) []zap.Field {
+	zapFields := make([]zap.Field, len(fields))
+
+	for i, f := range fields {
+		switch v := f.Value.(type) {
+		case string:
+			zapFields[i] = zap.String(f.Key, v)
+		case int:
+			zapFields[i] = zap.Int(f.Key, v)
+		case error:
+			zapFields[i] = zap.Error(v)
+		default:
+			zapFields[i] = zap.Any(f.Key, v)
+		}
+	}
+	return zapFields
+}
